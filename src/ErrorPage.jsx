cebf6eeec4bc8a5fff271ca3b702afec7a643373
Erick <112605408+Ep1ore@users.noreply.github.com>
2024-06-20 20:54:05 -0600

    styles on ErrorPage

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
+import styles from './styles/errorPage.module.css';
 
 const ErrorPage = () => {
     return (
-        <div>
+        <div className={styles.errorDiv}>
             <h1>Page was not found</h1>
             <Link to="/">
                 Go back
